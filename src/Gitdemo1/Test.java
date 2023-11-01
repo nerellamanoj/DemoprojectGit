67fbbce622879f067eaca3f405196c532dcf94a5
Manoj Kumar Nerella <nerellakumarmanoj@gmail.com>
2023-11-01 16:05:26 +0530

    Add file

---
+++ src/Gitdemo1/Test.java
@@ -0,0 +1,21 @@
+package Gitdemo1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String Hungry = "yes";
+		
+		 if (Hungry == "yes");
+		 System.out.println(" Eat somthing Tasty");
+		 
+	}
+}
+		 
+		 
+		 
+		 
+               
+	
+
+
